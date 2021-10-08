f5adf505aa41c9b27f9de662f0339cdca58bb35c
Bodyancheq <bogdan_abarihin@mail.ru>
2021-10-08 14:28:04 +0300

    reworked project, registration and authorization done

--- flask_006/__init__.py
+++ flask_006/__init__.py
@@ -7,10 +7,20 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET KEY'] = 'iogjpoijofoslgokgosf'
+    app.config['SECRET_KEY'] = 'iogjpoijofoslgokgosf'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from flask_006.models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
